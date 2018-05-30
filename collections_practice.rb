014c5333c94bf8407015d1f368394752e39b9b9d
Giles Bonner <gjbonner@gmail.com>
2018-05-30 16:58:05 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
-# your code goes here
+require 'pry'
+def begins_with_r(tools)
+  i = 0
+  if block_given?
+    new_tools = []
+    while i < tools.length do
+      binding.pry
+      if tools[i] start_with?("r")
+        new_tools.push(yield tools[i])
+        i = i + 1
+      end
+    end
+  end
+  return new_tools
+  else
+  return "No block given"
+end
